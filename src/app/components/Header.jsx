8f3a4675c3c8a75e0f27b6249f1880fe188ee874
itspratyusha <pratyushastha37@gmail.com>
2026-03-01 22:37:30 +0545

    Add news fetching and update layout in Header and Page components

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,14 +4,12 @@ import React, { useEffect, useState } from 'react'
 
 function Header() {
   let [temp, setTemp] = useState([]);
-
+  let [news, setNews] = useState([])
   useEffect(() => {
-    fetch(
-      "https://api.open-meteo.com/v1/forecast?latitude=27.7&longitude=85.3&current_weather=true"
-    )
-      .then(a => a.json())
-      .then(data => {
-        setTemp(data.current_weather.temperature);
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=27.7&longitude=85.3&current_weather=true").then(a => a.json()).then(data => {setTemp(data.current_weather.temperature);
+      fetch(`https://newsapi.org/v2/everything?q=apple&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`)
+     .then(a => a.json())
+     .then(b => setNews(b.articles)) 
       })
   }, []);
 
@@ -37,9 +35,12 @@ function Header() {
           </ul>
         </div>
       </section>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+
+<nav className="navbar navbar-expand-lg bg-light navbar-light p-3">
+  <div className="container">
+    <a className="navbar-brand" href="#">
+      <img className='w-75' src="http://kiante.wowtheme7.com/wp-content/uploads/2022/04/cropped-Logo-Black.png" alt="" />
+    </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
@@ -48,20 +49,25 @@ function Header() {
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+         <li className="nav-item">
+          <a className="nav-link" href="#">Bussiness</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="#">Politics</a>
+        </li>
+      
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">Culture</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Sports</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
+More          </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="#">Science & Tech</a></li>
+            <li><a className="dropdown-item" href="#">Health</a></li>
           </ul>
         </li>
       </ul>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 export default function Page() {
 
-  const [news, setNews] = useState([])
+  let [news, setNews] = useState([])
 
   useEffect(() => {
     fetch(`https://newsapi.org/v2/everything?q=apple&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`)
@@ -12,6 +12,8 @@ export default function Page() {
     }, [])
 
   return (
+    <>
+    <section className='py-5'>
     <div className="container">
 <div className='row'>
       {/* LEFT SIDE */}
@@ -26,8 +28,8 @@ export default function Page() {
       </div>
       
       {/* RIGHT SIDE */}
-      <div className=" col-4">
-        <h2 className="sidebarTitle right">Most loved</h2>
+      <div className="right col-4">
+        <h2 className="sidebarTitle ">Most loved</h2>
 
         {news.slice(9, 13).map((a) => (
           <div key={a.url} className="sideItem">
@@ -49,6 +51,38 @@ export default function Page() {
           </div>
         ))}
       </div>
+<h3>Business</h3>
+      <div className="left d-flex gap-3">
+      <div className='row'>
+        {news.slice(19, 23).map((a) => (
+          <div className='col-lg-6 ' key={a.url}>
+            <img src={a.urlToImage} alt="" />
+            <h2>{a.title}</h2>
+            <p>{a.description}</p>
+          </div>
+        ))}
+      </div>
+       <div className="right col-4">
+        <h2 className="sidebarTitle ">Featured Posts</h2>
+
+        {news.slice(14, 18).map((a) => (
+          <div key={a.url} className="sideItem ">
+            <img className=' h-50 w-40' src={a.urlToImage} alt="" />
+            <div>
+              <h4>{a.title}</h4>
+              <p>{a.source.name}</p>
+            </div>
+            
+
+          </div>
+
+        ))}
+
+      </div>
+      
+      </div>
     </div>
+    </section>
+    </>
   )
 }
