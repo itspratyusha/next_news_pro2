84e4b682a4cd62ee339133226406c6ffa5debeeb
itspratyusha <pratyushastha37@gmail.com>
2026-07-29 09:01:28 +0545

    Update news fetching logic to use local API endpoint

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,9 +10,7 @@ export default function Page() {
   useEffect(() => {
     async function getNews() {
       console.log("Fetching news...");
-      const response = await fetch(
-  `https://gnews.io/api/v4/top-headlines?lang=en&country=us&max=10&apikey=${process.env.NEXT_PUBLIC_GNEWS_API_KEY}`
-);
+      const response = await fetch("/api/news");
      const data = await response.json();
 console.log(data.articles[0]);
 setNews(data.articles);
