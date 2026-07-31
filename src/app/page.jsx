b68ec27cdb1c56ffe0d229d318f60331a25530e8
itspratyusha <pratyushastha37@gmail.com>
2026-07-31 23:42:57 +0545

    Refactor news fetching logic and update displayed articles in sidebar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,10 +9,8 @@ export default function Page() {
 
   useEffect(() => {
     async function getNews() {
-      console.log("Fetching news...");
       const response = await fetch("/api/news");
      const data = await response.json();
-console.log(data.articles[0]);
 setNews(data.articles);
     }
     getNews();
@@ -38,7 +36,7 @@ setNews(data.articles);
             {/* RIGHT SIDE */}
             <div className="right col-4 ">
               <h2 className="sidebarTitle ">Most loved</h2>
-              {news.slice(2, 7).map((a) => (
+              {news.slice(7, 10).map((a) => (
                 <div key={a.url} className="sideItem o-lay">
                   <img src={a.image} alt="" />
                   <div>
